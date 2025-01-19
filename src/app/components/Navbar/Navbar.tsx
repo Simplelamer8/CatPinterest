39d98b22c5cc6a9f82a99a326fe6218cc388d210
Simplelamer8 <67641980+Simplelamer8@users.noreply.github.com>
2025-01-19 17:10:29 +0500

    Add files via upload

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -15,12 +15,12 @@ export default function Navbar() {
     <div className={styles.navbar}>
           <button 
             onClick={handleTabClick(1)} 
-            className={`p-5 ${activeTab === 1 ? 'bg-[#1E88E5]' : null}`} >
+            className={`p-5 ${activeTab === 1 ? 'bg-[#1E88E5] text-white' : null}`} >
               Все котики
           </button>
           <button 
             onClick={handleTabClick(2)} 
-            className={`p-5 ${activeTab === 2 ? 'bg-[#1E88E5]' : null}`}>
+            className={`p-5 ${activeTab === 2 ? 'bg-[#1E88E5] text-white' : null}`}>
               Любимые котики
           </button>
     </div>
